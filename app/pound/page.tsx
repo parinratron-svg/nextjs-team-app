e2508e373869bbda52329eb15dc45581f53d13a9
PNKittisak <kittisak.n1504@gmail.com>
2026-03-07 12:05:17 +0700

    feat: add profile page for Pound

--- app/pound/page.tsx
+++ app/pound/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function pound() { 
 return ( 
 <main style={{ padding: 24 }}> 
-<h1>pound Profile</h1> 
+<h1>Pound Profile</h1> 
 <p><b>Name:</b>Kittisak Nouanprachak</p> 
 <p><b>Student ID:</b>6720210100</p> 
 <p><b>Skills:</b> HTML, CSS, Git</p> 
