68f4f7976a08e4588f2385f2928221aa79ad3a39
puen <parinratron@gmail.com>
2026-03-07 11:52:22 +0700

    feat: add profile page for member-a

---
+++ app/puen/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function puen() {
+ return (
+ <main style={{ padding: 24 }}>
+ <h1>puen Profile</h1>
+ <p><b>Name:</b> Parinyatorn Nakin</p>
+ <p><b>Student ID:</b> 6720210043</p>
+ <p><b>Skills:</b> HTML, CSS, Git</p>
+ <Link href="">Back to Home</Link>
+ </main>
+ );
+}
