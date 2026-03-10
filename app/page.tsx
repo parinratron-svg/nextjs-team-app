354980e4bc9165452bc1c2b4a2fc3e4ace713409
puen <parinratron@gmail.com>
2026-03-10 18:43:00 +0700

    resolve merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
  return (
  <main style={{ padding: 24 }}>
- <h1>Team Portfolio - Developed by Student puen</h1>
+ <h1>Team Portfolio - Developed by Student   puen</h1>
  <h1>Team Portfolio Developed by leem</h1>
  <h1>Team Portfolio - Developed by Student Pound</h1>
  <p>Welcome to our team website.</p>
