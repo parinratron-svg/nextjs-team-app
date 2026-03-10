f6e6496026c1684bba636d78569329fbb3b3a5a6
Abdulhaleem <abdull90435@gmail.com>
2026-03-10 15:41:39 +0700

    feat: add profile page for leem

---
+++ app/leem/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"; 
+export default function leem() { 
+return ( 
+<main style={{ padding: 24 }}> 
+<h1>leem Profile</h1> 
+<p><b>Name:</b> Abdulhallem Srisuk</p> 
+<p><b>Student ID:</b> 6720210095</p> 
+<p><b>Skills:</b> HTML, CSS, Git</p> 
+<Link href="/">Back to Home</Link> 
+</main> 
+); 
+} 
