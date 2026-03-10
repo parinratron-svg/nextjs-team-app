bfb2cbc792aeb7d38316d54d575b6fbd57bbf9d2
PNKittisak <kittisak.n1504@gmail.com>
2026-03-10 17:17:31 +0700

    update landing heading

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Home() {
  return (
  <main style={{ padding: 24 }}>
- <h1>Team Portfolio</h1>
+ <h1>Team Portfolio - Developed by Student Pound</h1>
  <p>Welcome to our team website.</p>
  <ul>
- <li><a href="/puen">ปืน</a></li>
- <li><a href="/leem">ลีม</a></li>
- <li><a href="/pound">ปอนด์</a></li>
+ <li><a href="/puen">puen</a></li>
+ <li><a href="/leem">leem</a></li>
+ <li><a href="/pound">pound</a></li>
  </ul>
  </main>
  );
