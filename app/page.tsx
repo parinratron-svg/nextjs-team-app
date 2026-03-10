e9f91ec93492c53beee7553f99d1fd7dd7fbf60c
puen <parinratron@gmail.com>
2026-03-10 17:47:12 +0700

    update landing heading

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
  <h1>Team Portfolio - Developed by Student puen</h1>
  <p>Welcome to our team website.</p>
  <ul>
- <li><a href="/puen">puen</a></li>
- <li><a href="/leem">leem</a></li>
- <li><a href="/pound">pound</a></li>
+ <li><a href="/puen">ปืน</a></li>
+ <li><a href="/leem">ลีม</a></li>
+ <li><a href="/pound">ปอนด์</a></li>
  </ul>
  </main>
  );
